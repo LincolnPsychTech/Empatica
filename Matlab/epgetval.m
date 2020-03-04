b894b924d686ec0f2516b0b1603e284a61eba322
Todd Parsons <todd.e.parsons@googlemail.com>
2020-03-04 09:48:19 +0000

    getval placeholder

--- Matlab/epgetval.m
+++ Matlab/epgetval.m
@@ -4,11 +4,7 @@ if strcmp(sock.Status, 'closed')
 end
 try
     raw = fscanf(sock); % Get data back from socket
-    
-    
-    parsed = jsondecode(raw); % Parse json data to a structure
-    val = parsed.values.frame; % Remove extraneous layers
-    val.timestamp = datetime(val.timestamp); % Convert timestamps to datetime format
+    val = raw
 catch % If request fails...
     val = NaN; % Set value to NaN
     warning('Could not get value from sensor.') % Issue warning
