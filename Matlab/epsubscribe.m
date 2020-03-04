6af8d0ace89ecd0553a1559aa58ee04f14153aea
Todd Parsons <todd.e.parsons@googlemail.com>
2020-03-04 09:48:13 +0000

    Commenting

--- Matlab/epsubscribe.m
+++ Matlab/epsubscribe.m
@@ -1,11 +1,11 @@
 function msg = epsubscribe(sock, varargin)
 for s = varargin
-    s = lower(s{:});
+    s = lower(s{:}); % Convert to lowercase
     fprintf(sock, ['device_subscribe ' s ' ON\r']); % Send request to socket
     msg.(s) = fscanf(sock); % Get data back from socket
-    if contains(msg.(s), 'device_subscribe') && contains(msg.(s), 'OK')
-        disp(['Successfully subscribed to ' s])
-    else
-        warning(['Unable to subscribe to ' s])
+    if contains(msg.(s), 'device_subscribe') && contains(msg.(s), 'OK') % If successsful
+        disp(['Successfully subscribed to ' s]) % Notify user
+    else % If unsuccessful
+        warning(['Unable to subscribe to ' s]) % Notify user
     end    
 end
